cee48971567791e11daf8e43c5218270faa22566
Nikita Averyanov <rac1onal@yandex.ru>
2023-06-14 19:16:16 +0300

    feat: заполнить Auth.js

--- src/utils/Auth.js
+++ src/utils/Auth.js
@@ -1,33 +1,41 @@
-export const BASE_URL = 'https://api.nomoreparties.co';
+export const BASE_URL = 'https://auth.nomoreparties.co/';
 
-export const getContent = (token) => {
-  return fetch(`${BASE_URL}/users/me`, {
-    method: 'GET',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${token}`,
-    },
-  })
-  .then(res => res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`));
+function checkResponse(res) {
+	if (res.ok) {
+		return res.json();
+	};
+	return Promise.reject(`Ошибка: ${res.status}`);
 };
 
-export const register = () => {
-
+export function register({ email, password }) {
+	return fetch(`${BASE_URL}signup`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({ email, password }),
+	})
+		.then((res) => checkResponse(res));
 };
 
-export const login = (identifier, password) => {
-  return fetch(`${BASE_URL}/auth/local`, {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ identifier, password }),
-  })
-  .then(res => res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`));
+export function login({ email, password }) {
+	return fetch(`${BASE_URL}signin`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({ email, password })
+	})
+		.then((res) => checkResponse(res));
 };
 
-export const checkToken = () => {
-
+export function checkToken(myToken) {
+	return fetch(`${BASE_URL}users/me`, {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json',
+			'Authorization': `Bearer ${myToken}`,
+		},
+	})
+		.then((res) => checkResponse(res));
 };
